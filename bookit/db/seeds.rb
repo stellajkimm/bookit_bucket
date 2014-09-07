c83b90fd1b5dfc055af002248622d6969f6f174f
Stella Kim <stellajkimm@gmail.com>
2014-09-07 14:16:55 -0400

    started with seed

--- bookit/db/seeds.rb
+++ bookit/db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+Category.create(name: "Restaurants")
+Category.create(name: "Dates")
+Category.create(name: "Books")
+Category.create(name: "Miscellaneous")
+Category.create(name: "Movies")
+
+10.times do
+	location = Faker::Address.city + ", " + Faker::Address.state_abbr
+	user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number, password: "password", location: location)
+
+
+end
