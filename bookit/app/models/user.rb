b4222ac5fb59932490012e67e4ffd6e722fc6bf0
Stella Kim <stellajkimm@gmail.com>
2014-09-07 14:06:12 -0400

    added user validations

--- bookit/app/models/user.rb
+++ bookit/app/models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
   has_many :attendances
   has_many :attending_items, through: :attendances, source: :item
 
+  validates_presence_of :email
+  validates :email, uniqueness: true
+  validates :email, format: {with: /[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,}/}
+
 end
