372b0e771de9a1b984399b939e6b47e269826626
Stella Kim <stellajkimm@gmail.com>
2014-09-07 13:32:33 -0400

    added associations

--- bookit/app/models/bucket.rb
+++ bookit/app/models/bucket.rb
@@ -1,3 +1,6 @@
 class Bucket < ActiveRecord::Base
   belongs_to :category
+  belongs_to :creator, class_name: "User", foreign_key: :user_id
+  has_many :bucketownerships
+  has_many :owners, through: :bucketownerships, source: :user
 end
--- bookit/app/models/user.rb
+++ bookit/app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   acts_as_follower
   acts_as_followable
 
+  has_many :bucketownerships
+  has_many :owned_buckets, through: :bucketownerships, source: :bucket
+  has_many :created_buckets, class_name: "Bucket", foreign_key: :user_id
+  has_many :attendances
+  has_many :attending_items, through: :attendances, source: :item
+
 end
