23518d688716f9df1ccc653ce25d5074181504dc
Stella Kim <stellajkimm@gmail.com>
2014-09-08 23:25:00 -0400

    spec helper

--- bookit/db/seeds.rb
+++ bookit/db/seeds.rb
@@ -1,14 +1,19 @@
 require 'faker'
 
-Category.create(name: "Restaurants")
-Category.create(name: "Dates")
-Category.create(name: "Books")
-Category.create(name: "Miscellaneous")
-Category.create(name: "Movies")
+
+privacy = ["public", "private"]
+status = ""
+
+10.times do
+	Category.create(name: Faker::Commerce.department)
+end
 
 10.times do
 	location = Faker::Address.city + ", " + Faker::Address.state_abbr
 	user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number, password: "password", location: location)
 
-
+	2.times do
+		bucket = user.created_buckets.create(name: Faker::Commerce.product_name, privacy: privacy.sample, category_id: (1..10).to_a.sample)
+		bucket.items.create(name: Faker::Company.bs, status: status.sample)
+	end
 end
