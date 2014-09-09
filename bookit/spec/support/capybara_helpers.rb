0ac41696ace9554333fed80512b13465781a2b69
Stella Kim <stellajkimm@gmail.com>
2014-09-08 23:30:50 -0400

    capybara helpers

---
+++ bookit/spec/support/capybara_helpers.rb
@@ -0,0 +1,10 @@
+capybara_helpers.rbmodule CapybaraHelpers
+  def wait_for_ajax_to_finish
+    page.document.synchronize do
+      raise AjaxStillWorking unless page.evaluate_script('jQuery.active == 0')
+    end
+  end
+end
+
+
+class AjaxStillWorking < Capybara::ElementNotFound;end
