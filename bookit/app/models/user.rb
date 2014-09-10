7a4e2d1fd7fcfa9731a7e123b9a3d9748ebc2bd1
Stella Kim <stellajkimm@gmail.com>
2014-09-09 20:56:14 -0400

    took out bcrypt.. added user fields into devise user

--- bookit/app/models/user.rb
+++ bookit/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_secure_password
+  # has_secure_password
   acts_as_follower
   acts_as_followable
 
