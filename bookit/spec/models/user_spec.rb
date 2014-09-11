64012b6a8ef4a2da109553e6024be016c882816a
Stella Kim <stellajkimm@gmail.com>
2014-09-11 17:34:51 -0400

    fixed association spec for user model

--- bookit/spec/models/user_spec.rb
+++ bookit/spec/models/user_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe User, :type => :model do
   context "validations" do
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
+    # validate format of email
   end
 
   context "associations" do
-    it { should have_many :bucketownerships }
-		it { should have_many :owned_buckets }
-    it { should have_many :created_buckets }
-    it { should have_many :attendances }
-  	it { should have_many :attending_items }
-
+    it { should have_many(:bucketownerships) }
+		it { should have_many(:owned_buckets).through(:bucketownerships).source(:bucket) }
+    it { should have_many(:created_buckets).class_name("Bucket").with_foreign_key(:user_id) }
+    it { should have_many(:attendances) }
+  	it { should have_many(:attending_items).through(:attendances).source(:item) }
   end
 end
