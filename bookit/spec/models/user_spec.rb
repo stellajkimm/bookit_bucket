33d8fe5fccb4669b0388ccae03a14e5180e91e52
Stella Kim <stellajkimm@gmail.com>
2014-09-11 17:26:02 -0400

    user model spec

--- bookit/spec/models/user_spec.rb
+++ bookit/spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let!(:user) { FactoryGirl.create(:user) }
+
+  context "validations" do
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+  end
+
+  context "associations" do
+    it { should have_many :bucketownerships }
+		it { should have_many :owned_buckets }
+    it { should have_many :created_buckets }
+    it { should have_many :attendances }
+  	it { should have_many :attending_items }
+
+  end
 end
