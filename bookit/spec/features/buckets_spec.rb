c8232028d342bc9948e4c9392aa312d9344c0a00
Stella Kim <stellajkimm@gmail.com>
2014-09-11 23:49:09 -0400

    bucket feature spec that doesnt work

---
+++ bookit/spec/features/buckets_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+describe "Bucket", :js => true do
+  let(:user) { create :user_with_buckets }
+  let(:buckets) { user.created_buckets }
+
+  describe "User can see all buckets" do
+    it "by visting the homepage" do
+      visit buckets_path
+      p "***"
+      p buckets
+      p "***"
+      ### strange. buckets show up but do not show up when testing.
+      buckets.each do |t|
+        expect(page).to have_content t.name
+      end
+    end
+  end
+end
