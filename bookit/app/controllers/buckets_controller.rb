b3a13f038cf38365104ce6dd488670c1a9971e61
Stella Kim <stellajkimm@gmail.com>
2014-09-12 01:51:54 -0400

    added items controller and made a home page for own buckets

--- bookit/app/controllers/buckets_controller.rb
+++ bookit/app/controllers/buckets_controller.rb
@@ -3,6 +3,10 @@ class BucketsController < ApplicationController
 		@buckets = Bucket.all
 	end
 
+	def home
+		@buckets = current_user.owned_buckets
+	end
+
 	def show
 		@bucket = Bucket.find(params[:id])
 		@items = @bucket.items
