f547650a23525a6e8559ab603013593735f0edab
Stella Kim <stellajkimm@gmail.com>
2014-09-12 13:14:36 -0400

    styled lists of buckets

--- bookit/app/helpers/bucket_helper.rb
+++ bookit/app/helpers/bucket_helper.rb
@@ -7,4 +7,9 @@ module BucketHelper
 	def user(user_id)
 		user = User.find(user_id)
 	end
+
+	def short_list(bucket_id)
+		bucket = Bucket.find(bucket_id)
+		items = bucket.items.limit(5)
+	end
 end
