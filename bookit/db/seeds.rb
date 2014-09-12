b1d6cf344a0c677eead99e210a5667ee96874c55
Stella Kim <stellajkimm@gmail.com>
2014-09-12 02:38:43 -0400

    changed seed so that attendee user_ids are chosen from the actual owners

--- bookit/db/seeds.rb
+++ bookit/db/seeds.rb
@@ -26,7 +26,7 @@ end
 		10.times do
 			item = bucket.items.create(name: Faker::Company.bs, status: status.sample)
 			(0..bucket.bucket_ownerships.length).to_a.sample.times do
-				item.attendances.create(user_id: (1..bucket.bucket_ownerships.length).to_a.sample)
+				item.attendances.create(user_id: bucket.bucket_ownerships.sample.user_id)
 			end
 		end
 	end
@@ -51,7 +51,7 @@ user = User.create(first_name: "Stella", last_name: "Kim", email: "stella@stella
 	10.times do
 		item = bucket.items.create(name: Faker::Company.bs, status: status.sample)
 		(0..bucket.bucket_ownerships.length).to_a.sample.times do
-			item.attendances.create(user_id: (1..bucket.bucket_ownerships.length).to_a.sample)
+			item.attendances.create(user_id: bucket.bucket_ownerships.sample.user_id)
 		end
 	end
 end
