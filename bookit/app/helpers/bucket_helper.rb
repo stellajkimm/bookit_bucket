c7b730c54314e29f0fc6ec7218cc9008e55b44bd
Stella Kim <stellajkimm@gmail.com>
2014-09-13 17:37:30 -0400

    refactored out ridiculous bucket-username helpers

--- bookit/app/helpers/bucket_helper.rb
+++ bookit/app/helpers/bucket_helper.rb
@@ -1,13 +1,4 @@
 module BucketHelper
-	def username(user_id)
-		user = User.find(user_id)
-		user.first_name + " " + user.last_name
-	end
-
-	def user(user_id)
-		user = User.find(user_id)
-	end
-
 	def short_list(bucket_id)
 		bucket = Bucket.find(bucket_id)
 		items = bucket.items.limit(5)
