5dc821a2d9179c25a11f40231d961a0f00e1e726
Stella Kim <stellajkimm@gmail.com>
2014-09-25 16:44:42 -0400

    cleaned up comments from buckets controller

--- bookit/app/controllers/buckets_controller.rb
+++ bookit/app/controllers/buckets_controller.rb
@@ -11,14 +11,9 @@ class BucketsController < ApplicationController
 	end
 
 	def show
-		# @bucket = Bucket.find(params[:id])
 		@items = @bucket.items.order(status: :desc)
 		@item = Item.new
 	end
-
-	# def new
-	# 	@new_bucket = Bucket.new
-	# end
 
 	def create
 		@new_bucket = current_user.created_buckets.create(bucket_params)
@@ -26,25 +21,21 @@ class BucketsController < ApplicationController
 	end
 
 	def destroy
-		# bucket = Bucket.find(params[:id])
 		bucket.destroy
 		redirect_to buckets_home_path
 	end
 
 	def edit
-		# @bucket = Bucket.find(params[:id])
 		@hashtags = ""
 		@bucket.hashtags.each {|x| @hashtags << "##{x.tag} " }
 	end
 
 	def update
-		# @bucket = Bucket.find(params[:id])
 		@bucket.update(bucket_params)
 		redirect_to bucket_update_hashtags_path(@bucket, :hashtags => params[:bucket][:hashtags][:tag])
 	end
 
 	def archive
-		# bucket = Bucket.find(params[:id])
 		bucket.archive == false ? bucket.update(archive: true) : bucket.update(archive: false)
 		redirect_to buckets_home_path
 	end
