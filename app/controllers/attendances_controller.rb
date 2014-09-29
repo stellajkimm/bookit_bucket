c102fe0b4edc7ef6aff17c7211d4e43fdfb64e9e
Stella Kim <stellajkimm@gmail.com>
2014-09-29 18:48:27 -0400

    attendances work

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,21 +1,21 @@
 class AttendancesController < ApplicationController
 
 	def create
-		@item = Item.find(params[:id])
-		@item.attendances.create(attendances_params)
+		@item = Item.find(params[:item_id])
+		@item.attendances.create(user_id: current_user.id)
 		redirect_to bucket_path(@item.bucket)
 	end
 
 	def destroy
-		@item = Item.find(params[:id])
-		@attendance = @bucket.attendances.find_by(user_id: params[:attendance][:user_id])
+		@item = Item.find(params[:item_id])
+		@attendance = @item.attendances.find_by(user_id: current_user.id)
 		@attendance.destroy
 		redirect_to bucket_path(@item.bucket)
 	end
 
-	private
+	# private
 	
-	def attendances_params
-    params.require(:attendance).permit(:user_id)
-  end
+	# def attendances_params
+ #    params.require(:attendances).permit(:user_id)
+ #  end
 end
