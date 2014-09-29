fd14e0c5428b874d072f988e45517783c034672b
Stella Kim <stellajkimm@gmail.com>
2014-09-29 16:34:38 -0400

    added opaciy to hovering buckets

--- app/assets/javascripts/bucket.js
+++ app/assets/javascripts/bucket.js
@@ -3,14 +3,21 @@ $(document).ready(function(){
 	// $.each($('.panel-group').children(), function() {
 	// 	console.log($(this).find('.collapse').hasClass('.in'))
 	// })
+	$.each($('.container .bucket'), function() {
+		$(this).hover(function() {
+  		$(this).css('opacity', '.5') },
+  		function() {
+  		$(this).css('opacity', '1') })
+	})
 })
 
-var Collapse2 = function (element) {
-	this.$element = $(element)
-}
 
-Collapse2.prototype.show = function () { }
-Collapse2.prototype.hide = function () { }
-Collapse2.prototype.toggle = function () { 
-	this[this.$element.hasClass('in') ? 'hide' : 'show']()
-}
+// var Collapse2 = function (element) {
+// 	this.$element = $(element)
+// }
+
+// Collapse2.prototype.show = function () { }
+// Collapse2.prototype.hide = function () { }
+// Collapse2.prototype.toggle = function () { 
+// 	this[this.$element.hasClass('in') ? 'hide' : 'show']()
+// }
