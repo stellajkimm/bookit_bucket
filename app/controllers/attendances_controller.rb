6a571686993268620f3e584692d8408c30ac8d6a
Stella Kim <stellajkimm@gmail.com>
2014-09-29 18:49:11 -0400

    made instance variables into normal variables

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,16 +1,16 @@
 class AttendancesController < ApplicationController
 
 	def create
-		@item = Item.find(params[:item_id])
-		@item.attendances.create(user_id: current_user.id)
-		redirect_to bucket_path(@item.bucket)
+		item = Item.find(params[:item_id])
+		item.attendances.create(user_id: current_user.id)
+		redirect_to bucket_path(item.bucket)
 	end
 
 	def destroy
-		@item = Item.find(params[:item_id])
-		@attendance = @item.attendances.find_by(user_id: current_user.id)
-		@attendance.destroy
-		redirect_to bucket_path(@item.bucket)
+		item = Item.find(params[:item_id])
+		attendance = item.attendances.find_by(user_id: current_user.id)
+		attendance.destroy
+		redirect_to bucket_path(item.bucket)
 	end
 
 	# private
