a46ad80c4ed4c0d19156b38b4f45c23ac9290989
Stella Kim <stellajkimm@gmail.com>
2014-09-30 00:42:15 -0400

    made buckets#home include following

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -4,10 +4,13 @@ class BucketsController < ApplicationController
 
 	def index
 		@buckets = Bucket.where(archive: false, privacy: "public")
+		@number_of_buckets = @buckets.size
 	end
 
 	def home
 		@buckets = current_user.owned_buckets.where(archive: false)
+		@followed_buckets = current_user.followed_buckets
+		@number_of_buckets = @buckets.size + @followed_buckets.size
 		@type_of_bucket = ""
 	end
 
@@ -39,29 +42,34 @@ class BucketsController < ApplicationController
 
 	def archive
 		@bucket.archive == false ? @bucket.update(archive: true) : @bucket.update(archive: false)
+		@number_of_buckets = @buckets.size
 		redirect_to bucket_show_archive_path
 	end
 
 	def show_archive
 		@buckets = current_user.owned_buckets.where(archive: true)
+		@number_of_buckets = @buckets.size
 		@type_of_bucket = "archive"
 		render 'home'
 	end
 
 	def show_public
 		@buckets = current_user.owned_buckets.where(archive: false, privacy: "public")
+		@number_of_buckets = @buckets.size
 		@type_of_bucket = "public"
 		render 'home'
 	end
 
 	def show_private
 		@buckets = current_user.owned_buckets.where(archive: false, privacy: "private")
+		@number_of_buckets = @buckets.size
 		@type_of_bucket = "private"
 		render 'home'
 	end
 
 	def show_group
 		@buckets = current_user.owned_buckets.where(archive: false, privacy: "group")
+		@number_of_buckets = @buckets.size
 		@type_of_bucket = "group"
 		render 'home'
 	end
@@ -69,6 +77,7 @@ class BucketsController < ApplicationController
 	def show_following
 		# this code is show_group code. it is wrong and you need to update this.
 		@buckets = current_user.followed_buckets.where(archive: false)
+		@number_of_buckets = @buckets.size
 		@type_of_bucket = "following"
 		render 'home'
 	end
