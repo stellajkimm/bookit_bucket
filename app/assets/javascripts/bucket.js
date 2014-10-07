8c2dd97f74b5f5bc7ba129e2a846bf5cf37d652c
Stella Kim <stellajkimm@gmail.com>
2014-10-07 00:00:52 -0400

    bucket background works

--- app/assets/javascripts/bucket.js
+++ app/assets/javascripts/bucket.js
@@ -29,12 +29,14 @@ function itemClick(el) {
 function bucketHover(el) {
 	var color = $(el).css('background-color')
 	$(el).hover(function() {
-  		$(this).css('background-color', color.replace(')','').replace('rgb', 'rgba') + ", .3)")
-  		$(this).find('.bucket-content').css('opacity', '.50')
+  		// $(this).css('background-color', color.replace(')','').replace('rgb', 'rgba') + ", .3)")
+  		$(this).find('.bucket-background').css('opacity', '.3')
+  		$(this).find('.bucket-content').css('opacity', '.30')
   		$(this).find('.bucket-links').show()
   		$(this).find('.bucket-owners').show() },
   		function() {
   		$(this).css('background-color', color)
+  		$(this).find('.bucket-background').css('opacity', '1')
   		$(this).find('.bucket-content').css('opacity', '1')
   		$(this).find('.bucket-links').hide()
   		$(this).find('.bucket-owners').hide() })
